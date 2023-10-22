2efcea479e8044c87d345a08ef1906a533b21bbc
YegorkaNS <egorrnnik@gmail.com>
2023-10-23 03:38:25 +0500

    Выполнил базовую задачу в 3 спринте

--- TapButton/ViewController.swift
+++ TapButton/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func button(_ sender: Any) {
+     count += 1
+        label.text = "\(count)"
+    }
 }
 
