b022081e9c271f9628c1efdbdb7b75a4439ce79b
YegorkaNS <egorrnnik@gmail.com>
2023-10-23 21:30:34 +0500

    Исправлены замечание ревьювюера к задаче 3 Спринта

--- TapButton/ViewController.swift
+++ TapButton/ViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
-    var count = 0
+    @IBOutlet private weak var label: UILabel!
+    private var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func button(_ sender: Any) {
-     count += 1
+    @IBAction private func button(_ sender: Any) {
+        count += 1
         label.text = "\(count)"
     }
 }
